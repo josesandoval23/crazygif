bdd662d98141d05e906a9e7d8f42050d762d1eb1
julian sandoval <julian_0264@icloud.com>
2021-02-03 20:43:03 -0600

    updated http to https

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category) => {
-  const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=100&api_key=1Yhq8RpKpJE82VxECghHe40ei1m9DvXJ`;
+  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=100&api_key=1Yhq8RpKpJE82VxECghHe40ei1m9DvXJ`;
   const resp = await fetch(url);
   const { data } = await resp.json();
   const gifs = data.map(img => {
